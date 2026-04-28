1b4890bd15fafabb413a78e5e528e2dcd63d92df
Raffaele De Pasca <depascaraffaele@gmail.com>
2026-04-28 16:12:42 +0200

    aggiunta dinamicità ai serviceLinks e la footbar

--- src/components/FooterBar.jsx
+++ src/components/FooterBar.jsx
@@ -1,18 +1,27 @@
 import FooterBtn from "./FooterBtn";
 
+const socialLinks = [
+  { id: 1, name: "Facebook", href: "#", img: "/img/footer-facebook.png" },
+  { id: 2, name: "Twitter", href: "#", img: "/img/footer-twitter.png" },
+  { id: 3, name: "YouTube", href: "#", img: "/img/footer-youtube.png" },
+  { id: 4, name: "Pinterest", href: "#", img: "/img/footer-pinterest.png" },
+  { id: 5, name: "Periscope", href: "#", img: "/img/footer-periscope.png" },
+];
+
 function FooterBar() {
   return (
     <div className="footer-bar">
       <FooterBtn />
       <div className="follow-us">
         <span>Follow Us</span>
-        <img src="/img/footer-facebook.png" alt="Facebook" />
-        <img src="/img/footer-twitter.png" alt="Twitter" />
-        <img src="/img/footer-youtube.png" alt="YouTube" />
-        <img src="/img/footer-pinterest.png" alt="Pinterest" />
-        <img src="/img/footer-periscope.png" alt="Periscope" />
+        {socialLinks.map((social) => (
+          <a key={social.id} href={social.href} aria-label={social.name}>
+            <img src={social.img} alt={social.name} />
+          </a>
+        ))}
       </div>
     </div>
-  )
+  );
 }
+
 export default FooterBar;
--- src/components/ServiceLinks.jsx
+++ src/components/ServiceLinks.jsx
@@ -1,12 +1,54 @@
+const serviceLinks = [
+  {
+    id: 1,
+    label: "Digital Comics",
+    href: "#",
+    img: "/img/buy-comics-digital-comics.png",
+    alt: "Digital Comics",
+  },
+  {
+    id: 2,
+    label: "DC Merchandise",
+    href: "#",
+    img: "/img/buy-comics-merchandise.png",
+    alt: "DC Merchandise",
+  },
+  {
+    id: 3,
+    label: "Subscription",
+    href: "#",
+    img: "/img/buy-comics-subscriptions.png",
+    alt: "Subscription",
+  },
+  {
+    id: 4,
+    label: "Comic Shop Locator",
+    href: "#",
+    img: "/img/buy-comics-shop-locator.png",
+    alt: "Comic Shop Locator",
+  },
+  {
+    id: 5,
+    label: "DC Power Visa",
+    href: "#",
+    img: "/img/buy-dc-power-visa.svg",
+    alt: "DC Power Visa",
+  },
+];
+
 function ServiceLinks() {
   return (
     <ul className="service-links">
-      <li><img src="/img/buy-comics-digital-comics.png" alt="Digital Comics" /><span>Digital Comics</span></li>
-      <li><img src="/img/buy-comics-merchandise.png" alt="DC Merchandise" /><span>DC Merchandise</span></li>
-      <li><img src="/img/buy-comics-subscriptions.png" alt="Subscription" /><span>Subscription</span></li>
-      <li><img src="/img/buy-comics-shop-locator.png" alt="Comic Shop Locator" /><span>Comic Shop Locator</span></li>
-      <li><img src="/img/buy-dc-power-visa.svg" alt="DC Power Visa" /><span>DC Power Visa</span></li>
+      {serviceLinks.map((item) => (
+        <li key={item.id}>
+          <a href={item.href}>
+            <img src={item.img} alt={item.alt} />
+            <span>{item.label}</span>
+          </a>
+        </li>
+      ))}
     </ul>
-  )
+  );
 }
+
 export default ServiceLinks;

76782b0ff8de0ff6b530d679d96a67ffd8d343f9
Raffaele De Pasca <depascaraffaele@gmail.com>
2026-04-29 16:51:19 +0200

    eliminato component NavBar per inutilizzo + modificato NavLinks e Header con l'utlizzo dei pros

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
 import NavLinks from "./NavLinks";
 
+const navLinks = [
+  { id: 1, label: "Characters", href: "#", active: false },
+  { id: 2, label: "Comics", href: "#", active: true },
+  { id: 3, label: "Movies", href: "#", active: false },
+  { id: 4, label: "TV", href: "#", active: false },
+  { id: 5, label: "Games", href: "#", active: false },
+  { id: 6, label: "Collectibles", href: "#", active: false },
+  { id: 7, label: "Videos", href: "#", active: false },
+  { id: 8, label: "Fans", href: "#", active: false },
+  { id: 9, label: "News", href: "#", active: false },
+  { id: 10, label: "Shop", href: "#", active: false },
+];
+
 function Header() {
   return (
     <header className="site-header">
       <div className="container-xl d-flex align-items-center justify-content-between">
         <img src="/img/dc-logo.png" alt="DC Logo" />
-        <NavLinks />
+        <NavLinks links={navLinks}/>
       </div>
     </header>
   );
--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,20 +1,7 @@
-const navLinks = [
-  { id: 1, label: "Characters", href: "#", active: false },
-  { id: 2, label: "Comics", href: "#", active: true },
-  { id: 3, label: "Movies", href: "#", active: false },
-  { id: 4, label: "TV", href: "#", active: false },
-  { id: 5, label: "Games", href: "#", active: false },
-  { id: 6, label: "Collectibles", href: "#", active: false },
-  { id: 7, label: "Videos", href: "#", active: false },
-  { id: 8, label: "Fans", href: "#", active: false },
-  { id: 9, label: "News", href: "#", active: false },
-  { id: 10, label: "Shop", href: "#", active: false },
-];
-
-function NavLinks() {
+function NavLinks({links}) {
   return (
     <ul className="nav-links mb-0">
-      {navLinks.map((item) => (
+      {links.map((item) => (
         <li key={item.id}>
           <a href={item.href} className={item.active ? "active" : ""}>
             {item.label}

737475275c39895856cf88d1f6af1c63bd7a2113
Raffaele De Pasca <depascaraffaele@gmail.com>
2026-04-29 16:53:44 +0200

    modifica su ServiceBar e ServiceLinks per utilizzo props

--- src/components/ServiceBar.jsx
+++ src/components/ServiceBar.jsx
@@ -1,10 +1,48 @@
 import ServiceLinks from "./ServiceLinks";
 
+const serviceLinks = [
+  {
+    id: 1,
+    label: "Digital Comics",
+    href: "#",
+    img: "/img/buy-comics-digital-comics.png",
+    alt: "Digital Comics",
+  },
+  {
+    id: 2,
+    label: "DC Merchandise",
+    href: "#",
+    img: "/img/buy-comics-merchandise.png",
+    alt: "DC Merchandise",
+  },
+  {
+    id: 3,
+    label: "Subscription",
+    href: "#",
+    img: "/img/buy-comics-subscriptions.png",
+    alt: "Subscription",
+  },
+  {
+    id: 4,
+    label: "Comic Shop Locator",
+    href: "#",
+    img: "/img/buy-comics-shop-locator.png",
+    alt: "Comic Shop Locator",
+  },
+  {
+    id: 5,
+    label: "DC Power Visa",
+    href: "#",
+    img: "/img/buy-dc-power-visa.svg",
+    alt: "DC Power Visa",
+  },
+];
+
 function ServiceBar() {
   return (
     <section className="service-bar">
       <div className="container-xl">
-        <ServiceLinks />
+        <ServiceLinks links={serviceLinks} />
       </div>
     </section>
   );
--- src/components/ServiceLinks.jsx
+++ src/components/ServiceLinks.jsx
@@ -1,45 +1,9 @@
-const serviceLinks = [
-  {
-    id: 1,
-    label: "Digital Comics",
-    href: "#",
-    img: "/img/buy-comics-digital-comics.png",
-    alt: "Digital Comics",
-  },
-  {
-    id: 2,
-    label: "DC Merchandise",
-    href: "#",
-    img: "/img/buy-comics-merchandise.png",
-    alt: "DC Merchandise",
-  },
-  {
-    id: 3,
-    label: "Subscription",
-    href: "#",
-    img: "/img/buy-comics-subscriptions.png",
-    alt: "Subscription",
-  },
-  {
-    id: 4,
-    label: "Comic Shop Locator",
-    href: "#",
-    img: "/img/buy-comics-shop-locator.png",
-    alt: "Comic Shop Locator",
-  },
-  {
-    id: 5,
-    label: "DC Power Visa",
-    href: "#",
-    img: "/img/buy-dc-power-visa.svg",
-    alt: "DC Power Visa",
-  },
-];
 
-function ServiceLinks() {
+
+function ServiceLinks({links}) {
   return (
     <ul className="service-links row row-cols-1 row-cols-sm-2 row-cols-lg-5 g-4 mb-0 ps-0">
-      {serviceLinks.map((item) => (
+      {links.map((item) => (
         <li className="col" key={item.id}>
           <a href={item.href}>
             <img src={item.img} alt={item.alt} />
